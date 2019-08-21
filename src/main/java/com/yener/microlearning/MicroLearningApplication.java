2b5a8664e4ba7fe37c2a6b3d9c48822831c9d23b
yenerhan <yenerhan@gmail.com>
2019-08-21 23:16:24 +0300

    war oluşturuldu.

--- src/main/java/com/yener/microlearning/MicroLearningApplication.java
+++ src/main/java/com/yener/microlearning/MicroLearningApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFacto
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MicroLearningApplication {
+public class MicroLearningApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroLearningApplication.class, args);
@@ -27,5 +27,10 @@ public class MicroLearningApplication {
         Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
         factoryBean.setResources(new Resource[]{new ClassPathResource("project-init.json")});
         return factoryBean;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(MicroLearningApplication.class);
     }
 }
